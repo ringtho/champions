4c420ec1e0181add129929473eac50966982aebd
Smith Ringtho <sringtho@gmail.com>
2023-03-22 07:43:57 +0000

    Added ability to push the inputted value or endorsement to the firebase database

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js'
+
+
+const appSettings = {
+    databaseURL: "https://playground-550b2-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
 const listEl = document.getElementById("list")
 const publishBtn = document.getElementById("btn")
 let inputField = document.getElementById("input-field")
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementInDB = ref(database, "endorsements")
+
+console.log(endorsementInDB)
 
 
 publishBtn.addEventListener('click', () => {
@@ -8,6 +24,12 @@ publishBtn.addEventListener('click', () => {
     let inputValue = inputField.value
     newEl.textContent = inputValue
     listEl.appendChild(newEl)
-    inputField.value = ""
+    clearInputField()
+
+    push(endorsementInDB, inputValue)
 })
+
+function clearInputField(){
+    inputField.value = ""
+}
 
