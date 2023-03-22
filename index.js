50d66b7e39d58e895c7894a482676699c67d788b
Smith Ringtho <sringtho@gmail.com>
2023-03-22 07:06:23 +0000

    Added ability to add input text from the text area to the ul

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// javascript
+const listEl = document.getElementById("list")
+const publishBtn = document.getElementById("btn")
+let inputField = document.getElementById("input-field")
+
+
+publishBtn.addEventListener('click', () => {
+    let newEl = document.createElement("li")
+    let inputValue = inputField.value
+    newEl.textContent = inputValue
+    listEl.appendChild(newEl)
+    inputField.value = ""
+})
+
